a0ca8d6f645615054e6ef3cbee3a9bcd70a92b31
mishurmarina <mishurmarina@gmail.com>
2019-09-27 18:11:37 +0300

    Изменения AppData.expenses.answer1=answer2

--- lesson1/script.js
+++ lesson1/script.js
@@ -6,7 +6,7 @@ let person = {
 };
 confirm("Есть ли Вам 18?", "да");
 let answer = prompt("Есть ли Вам 18?", "да");
-console.log(typeof(person));
+console.log(typeof (person));
 
 let money = prompt("Ваш бюджет на месяц?", "");
 console.log(money);
@@ -14,11 +14,10 @@ let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 console.log(time);
 
 //let timeData;
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let answer11 = prompt("Во сколько обойдется?", "");
+
 
 let AppData = {
-    money,
+    budjet: money,
     timeData: time,
     expenses: {
         "answer1": "answer11"
@@ -27,9 +26,12 @@ let AppData = {
     income: [],
     savings: false
 };
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    answer2 = prompt("Во сколько обойдется?", ""),
 
-
-answer1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-answer11 = prompt("Во сколько обойдется?", "");
-console.log(answer1 + "  требуется  " + answer11 + "  рублей");
-alert("Бюджет на 1 день составляет  " + money / 30 + " рублей");
+    answer3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    answer4 = prompt("Во сколько обойдется?", "");
+console.log(answer1 + "  требуется  " + answer2 + "  рублей");
+AppData.expenses.answer1 = answer2;
+AppData.expenses.answer3 = answer4;
+alert("Бюджет на 1 день составляет  " + AppData.budjet / 30 + " рублей");
