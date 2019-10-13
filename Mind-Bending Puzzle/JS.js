4af92cbe07c30a913c8395a43941d77437d9a366
mishurmarina <mishurmarina@gmail.com>
2019-10-13 18:38:38 +0300

    Вопросы в дом.задании заданы с помощью цикла и добавлены Mind-Bending Puzzle и lesson2

---
+++ Mind-Bending Puzzle/JS.js
@@ -0,0 +1,5 @@
+console.log(null || 10 && 83 && 36 + 'лет');
+console.log( false - null + true + 19 + 'часов');
+console.log([] + 'Анусино' + '' + 17 + '.' + 10);
+console.log('всегда(' && (undefined || [0]) && 'ждём');
+alert('nothing)' && "приезжай");
--- lesson1/script.js
+++ lesson1/script.js
@@ -8,7 +8,7 @@ confirm("Есть ли Вам 18?", "да");
 let answer = prompt("Есть ли Вам 18?", "да");
 console.log(typeof (person));
 
-let money = prompt("Ваш бюджет на месяц?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 console.log(money);
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 console.log(time);
@@ -19,19 +19,69 @@ console.log(time);
 let AppData = {
     budjet: money,
     timeData: time,
-    expenses: {
-        "answer1": "answer11"
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    answer2 = prompt("Во сколько обойдется?", ""),
 
-    answer3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    answer4 = prompt("Во сколько обойдется?", "");
-console.log(answer1 + "  требуется  " + answer2 + "  рублей");
-AppData.expenses.answer1 = answer2;
-AppData.expenses.answer3 = answer4;
-alert("Бюджет на 1 день составляет  " + AppData.budjet / 30 + " рублей");
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+        a != '' && b != '' && a.length < 50) {
+
+
+        console.log("Done");
+        AppData.expenses[a] = b;
+    } else {
+        alert("Введите непустую строку");
+        a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+    }
+}
+let i = 0;
+/*while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+        a != '' && b != '' && a.length < 50) {
+
+
+        console.log("Done");
+        AppData.expenses[a] = b;
+
+    }
+    i++;
+}
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+        a != '' && b != '' && a.length < 50) {
+
+
+        console.log("Done");
+        AppData.expenses[a] = b;
+
+    }
+    i++;
+} while (i < 2);*/
+
+AppData.moneyPerDay = AppData.budjet / 30;
+alert("Ежедневный бюджет :  " + AppData.moneyPerDay + " рублей");
+
+if (AppData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (AppData.moneyPerDay > 100 &&
+    AppData.moneyPerDay < 1000) {
+    console.log("Средний уровень достатка");
+} else if (AppData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
