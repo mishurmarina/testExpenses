7e5d641513b1684c8775528ac3b4e968e967151d
mishurmarina <mishurmarina@gmail.com>
2020-05-14 11:47:09 +0300

    Реализация скрипта отправки данных

--- учебный9/project/js/script.js
+++ учебный9/project/js/script.js
@@ -161,4 +161,92 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    //Form:
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с Вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');//обычный формат
+       // request.setRequestHeader('Content-type','application/json;charset = utf - 8');/*заголо
+       // вок запроса в формате JSON*/
+
+        let formData = new FormData(form);
+
+        //для формата JSON создаем промежуточный объект:
+        // let obj = {};
+        // formData.forEach(function(value, Key){
+        //     obj[Key] = value;
+        // });
+        // let json = JSON.stringify(obj);
+       // request.send(json);// - поменяли тело запроса.
+
+        request.send(formData); // для обычного формата.
+
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0 ; i < input.length ; i++){
+            input[i].value = '';
+        }
+    });
+
+    //подключаем скрипт отправки данных к контактной форме:
+    let contactForm = document.querySelector('.contact-form'),
+    inputContactForm = contactForm.getElementsByTagName('input');
+
+    contactForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        contactForm.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        let contactFormData = new FormData();
+        
+            contactFormData.append('email',inputContactForm[0].value);
+            contactFormData.append('tel',inputContactForm[1].value);
+            
+        
+        request.send(contactFormData);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0 ; i < inputContactForm.length ; i++){
+            inputContactForm[i].value = '';
+        }
+
+    });
+
+
+
+
 });
